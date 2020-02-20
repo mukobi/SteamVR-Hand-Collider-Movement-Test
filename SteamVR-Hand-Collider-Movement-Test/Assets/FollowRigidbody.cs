8d37451e0644ab5b32b3fb5761b286db7410f0c2
Gabriel Mukobi <gabrielmukobi@gmail.com>
2020-02-19 19:13:08 -0800

    Set up initial scene

---
+++ SteamVR-Hand-Collider-Movement-Test/Assets/FollowRigidbody.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRigidbody : MonoBehaviour
+{
+    public Rigidbody target;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.transform.position;
+    }
+}
---
+++ SteamVR-Hand-Collider-Movement-Test/Assets/MovePositionForward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionForward : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speedMetersPerSec;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + transform.forward * speedMetersPerSec * Time.fixedDeltaTime);
+    }
+}
